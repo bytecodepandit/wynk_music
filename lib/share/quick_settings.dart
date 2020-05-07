7f3113dba7d4983d69ccea9f762da7350ce21029
devprakashbinary <38106321+devprakashbinary@users.noreply.github.com>
2020-05-07 08:48:34 +0530

    implemented cupertino action sheet and cupertino datetime picker

--- lib/share/quick_settings.dart
+++ lib/share/quick_settings.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
+
 
 class QuickSettings extends StatefulWidget {
   @override
@@ -7,6 +10,71 @@ class QuickSettings extends StatefulWidget {
 
 class _QuickSettingsState extends State<QuickSettings> {
   bool isSwitched = false;
+  String deliveryQuality = 'Hight';
+
+  /* call cupertino action sheet for changing delivery quality */
+  changeDeliveryQuality(BuildContext context) {
+    showCupertinoModalPopup(
+        context: context,
+        builder: (context) {
+          return CupertinoActionSheet(
+            title: Text('Change Music Quality'),
+            cancelButton: CupertinoActionSheetAction(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text('Cancel')
+            ),
+            actions: <Widget>[
+              CupertinoActionSheetAction(
+                onPressed: () {
+                  setState(() {
+                    deliveryQuality = 'HD';
+                  });
+                  Navigator.of(context).pop();
+                },
+                child: Text('HD'),
+              ),
+              CupertinoActionSheetAction(
+                onPressed: () {
+                  setState(() {
+                    deliveryQuality = 'High';
+                  });
+                  Navigator.of(context).pop();
+                },
+                child: Text('Hight'),
+              ),
+              CupertinoActionSheetAction(
+                onPressed: () {
+                  setState(() {
+                    deliveryQuality = 'Medium';
+                  });
+                  Navigator.of(context).pop();
+                },
+                child: Text('Medium'),
+              ),
+              CupertinoActionSheetAction(
+                onPressed: () {
+                  setState(() {
+                    deliveryQuality = 'Low';
+                  });
+                  Navigator.of(context).pop();
+                },
+                child: Text('Low'),
+              )
+            ],
+          );
+        }
+    );
+  }
+
+
+  selectSleepTime() {
+
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -20,14 +88,19 @@ class _QuickSettingsState extends State<QuickSettings> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text('Download Quality'),
-                    Text('High')
-                  ],
+                GestureDetector(
+                  onTap: () {
+                   changeDeliveryQuality(context);
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text('Download Quality'),
+                      Text('$deliveryQuality')
+                    ],
+                  ),
                 ),
-                SizedBox(height: 10),
+                SizedBox(height: 20),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
@@ -47,6 +120,7 @@ class _QuickSettingsState extends State<QuickSettings> {
                     )
                   ],
                 ),
+                SizedBox(height: 5),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
@@ -55,16 +129,24 @@ class _QuickSettingsState extends State<QuickSettings> {
                     child: Switch(
                       value: isSwitched,
                       onChanged: (value) {
-                        setState(() {
-                          isSwitched = value;
-                          print(isSwitched);
-                        });
+                        DatePicker.showDateTimePicker(context,
+                            showTitleActions: true,
+                            onChanged: (date) {
+                              print('change $date');
+                            }, onConfirm: (date) {
+                              print('confirm $date');
+                              setState(() {
+                                isSwitched = value;
+                                print(isSwitched);
+                              });
+                            }, currentTime: DateTime.now(), locale: LocaleType.en);
                       },
                       activeTrackColor: Colors.blue,
                       activeColor: Colors.white,
                     ),),
                   ],
                 ),
+                SizedBox(height: 10),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
